90991a44e04dad9d46e4f41747365685f0b1a3fd
Carlos Lopez <celm1990@gmail.com>
2025-04-03 16:37:54 -0500

    [TMP] account_payment_order: no importar res.bank por validaciones de BIC

--- account_payment_order/models/__init__.py
+++ account_payment_order/models/__init__.py
@@ -3,6 +3,7 @@ from . import account_payment_order
 from . import account_payment_line
 from . import account_move
 from . import account_move_line
-from . import res_bank
+
+# from . import res_bank
 from . import account_payment_method
 from . import account_payment
